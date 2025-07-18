0375a66897b4370a07861d999b9475c9a73bb888
fituby <x>
2025-07-18 20:10:57 +0200

    fix modlog for comms

--- modlog.js
+++ modlog.js
@@ -3,22 +3,22 @@ var modlog_items = {};
 function count_msgs(data, type, name, sup) {
     if (!data[type])
         return "";
-	let msgs = {};
-	for (const w of data[type]) {
+    let msgs = {};
+    for (const w of data[type]) {
         if (w in msgs)
             msgs[w]++;
         else
             msgs[w] = 1;
-	}
-	for (const msg of ["playbans", "appeal messages", "mod messages"]) {
+    }
+    for (const msg of ["playbans", "appeal messages", "mod messages"]) {
         if (msg in msgs && msgs[msg] == 1) {
             delete msgs[msg];
             msgs[msg.slice(0, -1)] = 1;
         }
     }
-	const txt_start = type == 'warning' ? 9 : 0;
-	let msg_list = [];
-	if (sup)
+    const txt_start = type == 'warning' ? 9 : 0;
+    let msg_list = [];
+    if (sup)
         $.each(msgs, function (key, val) {
             msg_list.push(`${val} ${key.slice(txt_start)}<br>`);
         });
@@ -26,12 +26,12 @@ function count_msgs(data, type, name, sup) {
         $.each(msgs, function (key, val) {
             msg_list.push(`<span style="display: flex; align-items: baseline;"><h2 style="padding-right: 5px;">${val}</h2>${key.slice(txt_start)}</span>`);
         });
-	if (!msg_list.length)
-	    return "";
-	return `<span style="padding: 0 20px 0 0">
-	            <h2>${name}<sup>${sup}</sup></h2>
-	            ${msg_list.join("")}
-	        </span>`;
+    if (!msg_list.length)
+        return "";
+    return `<span style="padding: 0 20px 0 0">
+                <h2>${name}<sup>${sup}</sup></h2>
+                ${msg_list.join("")}
+            </span>`;
 }
 
 const timeline_types = {action: "Actions", warning: "Warnings", mod_msg: "Mod msgs"};
@@ -42,7 +42,7 @@ function add_timeline_info(age, data) {
     $.each(timeline_types, function (type, name) {
         items.push(count_msgs(data, type, name, sup));
     });
-	return items.join("");
+    return items.join("");
 }
 
 const periods = ['Year', ''];
@@ -82,60 +82,60 @@ function add_modlog_info() {
             modlog_items[age]['action'].push($(this).text());
         });
 
-	$('.mod-timeline__event--modlog .mod-timeline__text:contains("Warning: ")').each(function(i, o) {
-	    const age = get_age($(this));
-		const texts = $(this).text().split(' / ');
-		for (let w of texts) {
-			if (w == "stalling on time")
-				continue;
-			const type = w.startsWith("Warning: ") ? 'warning' : 'mod_msg';
-			if (!(type in modlog_items[age]))
-				modlog_items[age][type] = [];
-			modlog_items[age][type].push(w);
-		}
-	});
+    $('.mod-timeline__event--modlog .mod-timeline__text:contains("Warning: ")').each(function(i, o) {
+        const age = get_age($(this));
+        const texts = $(this).text().split(' / ');
+        for (let w of texts) {
+            if (w == "stalling on time")
+                continue;
+            const type = w.startsWith("Warning: ") ? 'warning' : 'mod_msg';
+            if (!(type in modlog_items[age]))
+                modlog_items[age][type] = [];
+            modlog_items[age][type].push(w);
+        }
+    });
 
-	$('.mod-timeline__event--modlog .mod-timeline__event__action--modMessage:not(.mod-timeline__event__action--warning)').each(function(i, o) {
-	    const age = get_age($(this));
+    $('.mod-timeline__event--modlog .mod-timeline__event__action--modMessage:not(.mod-timeline__event__action--warning)').each(function(i, o) {
+        const age = get_age($(this));
         if (!('mod_msg' in modlog_items[age]))
             modlog_items[age]['mod_msg'] = [];
         modlog_items[age]['mod_msg'].push("mod messages");
-	});
+    });
 
-	$('.mod-timeline__event--playban > .mod-timeline__event__body:contains("Playban")').each(function(i, o) {
+    $('.mod-timeline__event--playban > .mod-timeline__event__body:contains("Playban")').each(function(i, o) {
         const num = parseInt($(this).text().split(" "));
         if (!num || num < 0 || num > 999)
             return;
-	    const age = get_age($(this));
+        const age = get_age($(this));
         if (!('action' in modlog_items[age]))
             modlog_items[age]['action'] = [];
         for (let i = 0; i < num; i++)
             modlog_items[age]['action'].push("playbans");
-	});
+    });
 
-	$('.mod-timeline__event__action--setKidMode, .mod-timeline__event__action--unsetKidMode').each(function(i, o) {
-	    const age = get_age($(this));
-	    if (!('action' in modlog_items[age]))
+    $('.mod-timeline__event__action--setKidMode, .mod-timeline__event__action--unsetKidMode').each(function(i, o) {
+        const age = get_age($(this));
+        if (!('action' in modlog_items[age]))
             modlog_items[age]['action'] = [];
         modlog_items[age]['action'].push("kid mode");
-	});
+    });
 
-	$('.mod-timeline__event--appeal > .mod-timeline__event__body > a.user-link > i.moderator').each(function(i, o) {
-	    const age = get_age($(this));
-	    if (!('mod_msg' in modlog_items[age]))
+    $('.mod-timeline__event--appeal > .mod-timeline__event__body > a.user-link > i.moderator').each(function(i, o) {
+        const age = get_age($(this));
+        if (!('mod_msg' in modlog_items[age]))
             modlog_items[age]['mod_msg'] = [];
         modlog_items[age]['mod_msg'].push("appeal messages");
-	});
+    });
 
     let items = [];
-	for (const age of ['new', '?age', 'old']) {
-	    if (age in modlog_items)
-	        items.push(add_timeline_info(age, modlog_items[age]));
-	}
-	const text = `<div id="pax-modlog-info" style="display: flex; align-items: top;">
+    for (const age of ['new', '?age', 'old']) {
+        if (age in modlog_items)
+            items.push(add_timeline_info(age, modlog_items[age]));
+    }
+    const text = `<div id="pax-modlog-info" style="display: flex; align-items: top;">
                      ${items.join("")}
-			     </div>`;
-	$(text).insertBefore('.mod-timeline');
+                 </div>`;
+    $(text).insertBefore('.mod-timeline');
 }
 
 var modlog_btns = {
@@ -227,22 +227,22 @@ function add_modlog_buttons() {
             </button>`);
             btn_ids.push(btn_id);
         }
-	});
-	const total = $('.mod-timeline__event').length;
-	btns.push(`<button id="pax-total" class="btn-rack__btn">Total: ${total}</button>`);
+    });
+    const total = $('.mod-timeline__event').length;
+    btns.push(`<button id="pax-total" class="btn-rack__btn">Total: ${total}</button>`);
     const pax_buttons = `<div id="pax-modlog-btns" class="btn-rack">${btns.join("")}</div>`;
-	$(pax_buttons).insertBefore('.mod-timeline');
+    $(pax_buttons).insertBefore('.mod-timeline');
     for (const btn_id of btn_ids) {
         $(`#${btn_id}`).click(() => { modlog_btn_clicked(btn_id); });
-	};
-	$('#pax-total').click(modlog_total_clicked);
+    };
+    $('#pax-total').click(modlog_total_clicked);
 
-	for (const btn_id of ['pax-appeals', 'pax-playbans', 'pax-blogs', 'pax-streamers'])
-	    if ($(`#${btn_id}`).length)
-	        modlog_btn_clicked(btn_id);
-	for (const btn_id of ['pax-reports', 'pax-flaglines'])
-	    if ($(`#${btn_id}`).length && !$(`${modlog_btns[btn_id].sel} .mod-timeline__report-form--open`).length)
-	        modlog_btn_clicked(btn_id);
+    for (const btn_id of ['pax-appeals', 'pax-playbans', 'pax-blogs', 'pax-streamers'])
+        if ($(`#${btn_id}`).length)
+            modlog_btn_clicked(btn_id);
+    for (const btn_id of ['pax-reports', 'pax-flaglines'])
+        if ($(`#${btn_id}`).length && !$(`${modlog_btns[btn_id].sel} .mod-timeline__report-form--open`).length)
+            modlog_btn_clicked(btn_id);
 }
 
 function split_presets() {
@@ -259,32 +259,35 @@ function split_presets() {
 const config_modlog = { attributes: false, childList: true, subtree: false };
 const mod_zone_callback = (mutationList, observer) => {
     modlog_items = {};
-	for (const mutation of mutationList) {
-		if (mutation.type === "childList") {
-			for (const node of mutation.addedNodes) {
-				if (node.id == "mz_timeline") {
-					setTimeout(() => {
+    for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+            for (const node of mutation.addedNodes) {
+                if (node.id == "mz_timeline") {
+                    setTimeout(() => {
                         if (!$('#pax-modlog-info').length) {
                             add_modlog_info();
                             add_modlog_buttons();
                             split_presets();
                         }
-					});
-					return;
-				}
-			}
-		}
-	}
+                    });
+                    return;
+                }
+            }
+        }
+    }
 };
 const observer_modlog = new MutationObserver(mod_zone_callback);
 $('.mod-zone').each(function(i, o) {
-    if ($(this).is('.none'))
+    if ($(this).is('.none')) {
         observer_modlog.observe(o, config_modlog);
+    }
     else {
         if (!$('#pax-modlog-info').length) {
-            add_modlog_info();
-            add_modlog_buttons();
-            split_presets();
+            setTimeout(() => {
+                add_modlog_info();
+                add_modlog_buttons();
+                split_presets();
+            });
         }
     }
 });
